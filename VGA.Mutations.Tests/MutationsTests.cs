72e40752efaf9b88351b1eba7411caaea02338b8
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-03-15 15:07:21 +0000

    Creation test failing.

---
+++ VGA.Mutations.Tests/MutationsTests.cs
@@ -0,0 +1,16 @@
+﻿namespace VGA.Mutations.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using TestAssembly;
+
+    [TestFixture]
+    public class MutationsTests
+    {
+        [Test]
+        public void WhenAMutationIsCreated_TheAssemblyShouldBeLoaded()
+        {
+            new Mutation().Create<Calculator>().Assembly.Should().NotBeNull();
+        }
+    }
+}
---
+++ VGA.Mutations/Mutation.cs
@@ -0,0 +1,16 @@
+﻿namespace VGA.Mutations
+{
+    public class Mutation
+    {
+        public AssemblyDefinition Assembly { get; set; }
+
+        public Mutation Create<T>() where T : class
+        {
+            return this;
+        }
+    }
+
+    public class AssemblyDefinition
+    {
+    }
+}
